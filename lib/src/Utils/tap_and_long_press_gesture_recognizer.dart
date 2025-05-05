54d39a5407b68bbc5e4946fac74dc2d4ccef5133
SayedZeeshanHyder <zhyder133@gmail.com>
2025-05-05 22:16:09 +0530

    Package Release 0.0.4 commit no 2

--- lib/src/Utils/tap_and_long_press_gesture_recognizer.dart
+++ lib/src/Utils/tap_and_long_press_gesture_recognizer.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
 
 class TapAndLongPressGestureRecognizer extends OneSequenceGestureRecognizer {
   final GestureTapCallback? onTap;
